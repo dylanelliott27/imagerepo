b85fb87b550bc85e4555d8e42c9cd593b0669d3e
dylan <dylanelliott27@hotmail.com>
2020-09-08 14:49:54 -0400

    not enough ram to build on vps

---
+++ front-end/build/precache-manifest.2be02b951a24b5a8b71c721b5f975d5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20590b1d98449a2550031370969d692d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40bb31739020203beb82",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "cb26b09921b68f52424c",
+    "url": "/static/css/main.50695a3d.chunk.css"
+  },
+  {
+    "revision": "40bb31739020203beb82",
+    "url": "/static/js/2.1859ade4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1859ade4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb26b09921b68f52424c",
+    "url": "/static/js/main.39c0f532.chunk.js"
+  },
+  {
+    "revision": "27c82225a3b010506d6e",
+    "url": "/static/js/runtime-main.dd9f32f8.js"
+  }
+]);
